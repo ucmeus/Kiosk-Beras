6ff7c7d532b0bc760f63f63e60fb76e2cf10af8c
yoe c me <44598467+ucmeus@users.noreply.github.com>
2026-04-07 14:12:35 +0700

    Add files via upload

--- database/kiosk_schema.sql
+++ database/kiosk_schema.sql
@@ -1,1 +1,18 @@
+-- Aplikasi ini menggunakan Google Sheets sebagai database utama.
 
+-- Struktur sheet yang dipakai otomatis dibuat oleh fungsi `setupApp_()` di Code.gs:
+-- 1) Setting
+-- 2) Users
+-- 3) BackupHistory
+-- 4) Supplier
+-- 5) KategoriBeras
+-- 6) Pelanggan
+-- 7) Saldo
+-- 8) CashFlow
+-- 9) Pembelian
+-- 10) Transaksi
+
+-- Langkah pakai:
+-- 1) Pastikan project Apps Script bound ke Google Spreadsheet.
+-- 2) Jalankan fungsi `setupApp` satu kali dari editor Apps Script.
+-- 3) Deploy ulang sebagai Web App.
